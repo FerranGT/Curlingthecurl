da8648d520461e6d9fdd1c6e9b84ee35e3b71542
Judit <Judith@MacBook-Air-de-Judith.local>
2016-11-17 15:25:58 +0100

    working with modal

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,45 +1,63 @@
-angular.module("myApp",['myServices'])
-	.controller('demoCtrl', ($scope, DataService) => {
+angular.module("myApp",['myServices','angularModalService','ngAnimate'])
+	.controller('productsCtrl', ($scope, DataService, ModalService) => {
+		
 		$scope.findProducts = () => {
 			DataService.getProducts()
 				.then( function(response) {
-					$scope.products = response.data;
-					console.log(response);
+					$scope.products = response.data.filter((elem) => {
+					 	if (elem.type=="producto" && !!elem.url) {return elem}
+					});
 				})
 		}
 
-	$scope.findProducts();
 
-})
+ 		$scope.showCustom = function() {
+ 			console.log("showing modal...")
+		    ModalService.showModal({
+		      templateUrl: "viewsClient/custom.html",
+		      controller: "CustomController"
+		    })
+		    .then(function(modal) {
+		      modal.close.then(function(result) {
+		        $scope.customResult = "All good!";
+		      });
+		    });
+
+		  };
+
+		$scope.findProducts();
+	})
+	.controller('CustomController', function($scope, close) {
+		$scope.close = close;
+	})
+	
 
 
 
 
 
+// .controller('DropdownCtrl', function ($scope, $log) {
+// 	$scope.items = [
+// 	'Servicio',
+// 	'Producto'
+// 	];
 
+// 	$scope.status = {
+// 		isopen: false
+// 	};
 
-	// .controller('DropdownCtrl', function ($scope, $log) {
-	// 	$scope.items = [
-	// 	'Servicio',
-	// 	'Producto'
-	// 	];
+// 	$scope.toggled = function(open) {
+// 		$log.log('Dropdown is now: ', open);
+// 	};
 
-	// 	$scope.status = {
-	// 		isopen: false
-	// 	};
+// 	$scope.toggleDropdown = function($event) {
+// 		$event.preventDefault();
+// 		$event.stopPropagation();
+// 		$scope.status.isopen = !$scope.status.isopen;
+// 	};
 
-	// 	$scope.toggled = function(open) {
-	// 		$log.log('Dropdown is now: ', open);
-	// 	};
-
-	// 	$scope.toggleDropdown = function($event) {
-	// 		$event.preventDefault();
-	// 		$event.stopPropagation();
-	// 		$scope.status.isopen = !$scope.status.isopen;
-	// 	};
-
-	// 	$scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
-	// 	$log.log($scope.appendToEl)
-	// });
+// 	$scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+// 	$log.log($scope.appendToEl)
+// });
 
 
