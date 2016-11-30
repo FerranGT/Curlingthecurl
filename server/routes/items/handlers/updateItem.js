dcc49c510ba6e0a970cc8d314b7baa7d103b8640
Judit <Judith@MacBook-Air-de-Judith.local>
2016-11-30 15:13:30 +0100

    update working after problem discovered with mongoose migration

--- server/routes/items/handlers/updateItem.js
+++ server/routes/items/handlers/updateItem.js
@@ -2,19 +2,11 @@ const Article = require('../../../models/articles')
 
 function updateItem (req,res) {
 	
-	const { id } = req.params;
+	const { _id, title, desc, price, type, url } = req.body;
 
-	Article.findByIdAndUpdate( id )
+	Article.findByIdAndUpdate( _id, { title, desc, price, type, url } )
 		.then( data => res.redirect('/create'))
 		.catch(console.log)
-
-	// const { _id, title, desc, price, type, url } = req.body;
-	// db.collection("articles")
-	// .update( 
-	// 	{ _id: ObjectID(_id) }, 
-	// 	{ $set: { title, desc, price, type, url } }
-	// )
-	// .then( data => res.redirect('/create'));
 }
 
 module.exports = updateItem;
