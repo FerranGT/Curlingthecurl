886c276fafd619b1c66bf1257414be8668779ba9
Judit <Judith@MacBook-Air-de-Judith.local>
2016-11-30 12:34:21 +0100

    changing routes

--- client/public/js/service.js
+++ client/public/js/service.js
@@ -2,29 +2,29 @@ angular.module('myServices',[])
 	.factory('DataService', function($http) {
 		
 		function getProducts() {
-			return $http.get("http://localhost:3000/api/articles");
+			return $http.get("/api/articles");
 		}
 
 		function deleteItem(id){
 			console.log("service: " + id);
-			return $http.delete("http://localhost:3000/items/" + id)
+			return $http.delete("/items/" + id)
 		}
 
 		function updateItem(id){
 			console.log("service: " + id);
-			return $http.put("http://localhost:3000/items/" + id)
+			return $http.put("/items/" + id)
 		}
 
 		function addDate(dateAppointment){
 			console.log("service: " + dateAppointment);
-			return $http.get("http://localhost:3000/appointments/show/" + dateAppointment)
+			return $http.get("/appointments/show/" + dateAppointment)
 		}
 
 		function newAppointment(selectedAppointment){
 			console.log("service appointment");
 			console.log(selectedAppointment);
 			const stringAppointment = JSON.stringify(selectedAppointment)
-			return $http.post("http://localhost:3000/appointments/", stringAppointment)
+			return $http.post("/appointments/", stringAppointment)
 
 		}
 
